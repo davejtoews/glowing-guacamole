9929dafca80e9e15515fd7d1c2793ebc607b270d
Dave Toews <davejtoews@gmail.com>
2016-03-05 15:52:00 -0700

    change business model

--- src/services/business/business-model.js
+++ src/services/business/business-model.js
@@ -4,10 +4,15 @@
 // for more of what you can do here.
 
 import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
 
 let businessSchema = new Schema({
   name: {type: String, required: true, index: true},
+  categories: [{type: ObjectId, ref: 'category'}],
+  address: {type: String},
+  lat: {type: Number},
+  lng: {type: Number},
   createdAt: {type: Date, 'default': Date.now},
   updatedAt: {type: Date, 'default': Date.now}
 });
