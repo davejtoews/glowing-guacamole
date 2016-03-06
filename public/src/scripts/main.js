8bcc6fe8f68aa36fa0e0ab4d0f23096000707ea7
Alyssa Quinney <alyssaquinney@Alyssas-MacBook-Pro.local>
2016-03-05 19:39:16 -0700

    added cool map styles and icons

--- public/src/scripts/main.js
+++ public/src/scripts/main.js
@@ -5,7 +5,19 @@ var plotlayers=[];
 var latitude_var = 10;
 var longitude_var = -80; 
 
+var chipIcon = L.icon({
+    iconUrl: 'public/src/img/chipIcon.png',
+    iconSize:     [107, 55], // size of the icon
+    iconAnchor:   [54, 53], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
+var businessIcon = L.icon({
+    iconUrl: 'public/src/img/businessIcon.png',
+    iconSize:     [108, 83], // size of the icon
+    iconAnchor:   [54, 83], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 if(!!navigator.geolocation){
 	navigator.geolocation.getCurrentPosition(function(position){
@@ -22,15 +34,14 @@ if(!!navigator.geolocation){
 
 function initmap(latitude_var, longitude_var) {
 	// set up the map
-	map = new L.Map('map');
-	// create the tile layer with correct attribution
-	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 20, attribution: osmAttrib});		
-	console.log(latitude_var, longitude_var);
-	
-	map.setView(new L.LatLng(latitude_var, longitude_var),17);
-	map.addLayer(osm);
+
+	L.mapbox.accessToken = 'pk.eyJ1Ijoib2JlNWVnaXJhZmZlIiwiYSI6ImNpbGZ4aDY5NDIyOWd2Zm1jajA1Ymx1dnkifQ.dURw54_s9uh7RBB8-E6NIA';
+	var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
+    attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	});
+
+	map = new L.Map('map').addLayer(mapboxTiles).setView(new L.LatLng(latitude_var, longitude_var),17);
+	L.marker([latitude_var, longitude_var], {icon: chipIcon}).addTo(map);
 }
 
 function markers(){
@@ -39,7 +50,7 @@ function markers(){
 	  console.log(data);
 	  data.forEach(function(datum){
 	  	console.log(datum);
-	  	L.marker([datum.lat, datum.lon])
+	  	L.marker([datum.lat, datum.lon], {icon: businessIcon})
 	  		.bindPopup(datum.details)
 	  		.addTo(map);
 	  });
